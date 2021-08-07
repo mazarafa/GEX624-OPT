0e3e7ed012c28d3b6dffc891df4e3197bd112411
Rafa <arruda.rafa.ra@gmail.com>
2021-08-07 02:26:40 -0400

    Update main4.cpp

--- seventh week/main4.cpp
+++ seventh week/main4.cpp
@@ -10,8 +10,7 @@ int main() {
 	while (tt--) {
 		int p;
 		cin >> p;
-		// it can be observed that the remainder of dividing 'p' by 2 and ('p' - 1) 
-		// is equal to 1, so the answer is always 2 and the number before 'p' ('p' - 1)
+		
 		cout << 2 << " " << p - 1 << '\n';
 	}
 	return 0;
