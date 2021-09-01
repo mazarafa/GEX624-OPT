53ee6f7bf6dc00586e91c606570106768f5d7f3d
mazarafa <arruda.rafa.ra@gmail.com>
2021-09-01 18:45:49 -0400

    resolutions of exercises in class.

---
+++ A9/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int p;
+		cin >> p;
+	
+		cout << 2 << " " << p - 1 << '\n';
+	}
+	return 0;
+}
